3d64a2db152b5ad6afa2fe5bf23c332be9dd1f58
cjolsen <christopherjolsen@gmail.com>
2015-06-15 21:37:25 -0600

    debugging database settings

--- usdarest/production_settings.py
+++ usdarest/production_settings.py
@@ -9,6 +9,7 @@ SECRET_KEY = 'y3m$=rs7$!91*31y*dt%m**ljam66otk4%ilm%d&b+y@lc7zvq'
 
 import dj_database_url
 DATABASES = {'default': dj_database_url.config()}
+DATABASES['default']['engine'] = 'django.db.backends.postgresql_psycopg2'
 
 # DATABASES = {
 #     'default': {
